6b8152f810c68553b384ae1fbe52a23a2f642990
Bentley G. Murphy <bentleygmurphy@Bentleys-MacBook-Pro.local>
2019-09-03 10:01:55 -0600

    Added simple alerts for faq

--- js/index.js
+++ js/index.js
@@ -1,1 +1,11 @@
-console.log("test")
+document.getElementById("whyBtn").addEventListener("click", alertWhy);
+
+function alertWhy() {
+    alert("We focus on making meals with the best and freshest ingredients, like humanely-raised beef that is ground fresh each day, organic chicken, and Rockey Family Dairy’s organic cream for our ice cream cones, sundaes and shakes. Our commitment to freshness and quality means higher prices than you’d find at traditional fast-food burger chains. It also means food that tastes way better — because, well, it is.");
+}
+
+document.getElementById("whereBtn").addEventListener("click", alertWhere);
+
+function alertWhere() {
+    alert("We source our humanely-raised, vegetarian-fed beef from local, family-owned ranches. We use an 85% to 15% protein to fat blend, a mixture of the lean protein and the marbling found on in a cut of steak for our ground beef. The mixture is ground daily, and thus, our burgers taste so amazing.");
+}
