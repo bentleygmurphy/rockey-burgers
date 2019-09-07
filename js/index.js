5f2e33fbadbc06c20501474f7ec283e3348ad654
Bentley G. Murphy <bentleygmurphy@Bentleys-MacBook-Pro.local>
2019-09-06 22:14:38 -0600

    Changed some class names for buttons, made join form only visible on click and made top section text overlay photo instead

--- js/index.js
+++ js/index.js
@@ -9,3 +9,10 @@ document.getElementById("whereBtn").addEventListener("click", alertWhere);
 function alertWhere() {
     alert("We source our humanely-raised, vegetarian-fed beef from local, family-owned ranches. We use an 85% to 15% protein to fat blend, a mixture of the lean protein and the marbling found on in a cut of steak for our ground beef. The mixture is ground daily, and thus, our burgers taste so amazing.");
 }
+
+document.getElementById("JoinBtn").addEventListener("click", showJoin);
+
+function showJoin() {
+    document.getElementById("joinForm").style.display = "flex";
+    document.getElementById("joinBtnPad").style.display = "none";
+}
