48bd0779fd743237ce82e9ad39326cf1c9357828
Bentley G. Murphy <bentleygmurphy@Bentleys-MacBook-Pro.local>
2019-09-18 10:38:34 -0600

    Made card animations and returnTop function on press

--- js/index.js
+++ js/index.js
@@ -17,18 +17,33 @@ function showJoin() {
 
 document.getElementById("JoinBtn").addEventListener("click", showJoin);
 
+function returnTop() {
+    let scrollToTop = window.setInterval(() => {
+        let pos = window.pageYOffset;
+        if (pos > 120) {
+            window.scrollTo(0, pos - 20);
+        } else {
+            window.clearInterval(scrollToTop);
+        }
+    }, 10);
+}
+
 function slcMap() {
     document.getElementById("gMap").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d193365.8053794238!2d-112.06056842352622!3d40.77652325568739!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x87523d9488d131ed%3A0x5b53b7a0484d31ca!2sSalt%20Lake%20City%2C%20UT!5e0!3m2!1sen!2sus!4v1568064364772!5m2!1sen!2sus"
+    returnTop();
 }
 
 function sandyMap() {
     document.getElementById("gMap").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48489.680035933416!2d-111.8842296048132!3d40.57239926278136!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x875264f8c14e6e49%3A0x5fe27acb10715499!2sSandy%2C%20UT!5e0!3m2!1sen!2sus!4v1568064417128!5m2!1sen!2sus"
+    returnTop();
 }
 
 function lehiMap() {
     document.getElementById("gMap").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97209.51389090667!2d-111.93883527990552!3d40.41334081915472!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d7fedd81a1b69%3A0xfd6fac81600d61c9!2sLehi%2C%20UT!5e0!3m2!1sen!2sus!4v1568064463396!5m2!1sen!2sus"
+    returnTop();
 }
 
 function provoMap() {
     document.getElementById("gMap").src = "https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d97434.55345559241!2d-111.70894838454362!3d40.257313788050105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x874d9271930bf1bf%3A0x1d90f12600b556ef!2sProvo%2C%20UT!5e0!3m2!1sen!2sus!4v1568064501406!5m2!1sen!2sus"
+    returnTop();
 }
